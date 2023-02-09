36b2b55c0446b5a2cd6b58d15d7cd9a25313f208
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2023-02-09 19:10:34 -0300

    chore: api structure

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import cors from "cors"
+import "dotenv/config"
+import express from "express"
+
+import router from "@/routes"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(router)
+
+export default app
