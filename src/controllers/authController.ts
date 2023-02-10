b15c036571de0363b31558c9d81a55a0093a0aa6
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2023-02-10 04:55:57 -0300

    refactor: now sign-up route just returns status code

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -5,8 +5,8 @@ import { authService } from "@/services"
 const signUp = async (req: Request, res: Response) => {
 	const userData = req.body
 
-	const user = await authService.createUser(userData)
-	res.status(201).send(user)
+	await authService.createUser(userData)
+	res.sendStatus(201)
 }
 
 export default {
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,10 +2,14 @@ import { userRepository } from "@/respositories"
 import { ConflictError } from "@/errors"
 import { UserData } from "@/types/userTypes"
 
-const createUser = async (user: UserData): Promise<UserData> => {
+const createUser = async (user: UserData): Promise<void> => {
 	if (await userExists(user.cpf))
 		throw new ConflictError("User already exists")
-	return userRepository.create(user)
+
+	await userRepository.create({
+		...user,
+		birthDate: new Date(user.birthDate),
+	})
 }
 
 const userExists = async (username: string): Promise<UserData | null> => {
